3b6f36045617bfd1b92b1545825901a7b1cc0225
Hatem-Aiman <47148176+Hatem-Aiman@users.noreply.github.com>
2024-05-21 04:57:53 +0300

    updates

--- Bus-Reservation/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ Bus-Reservation/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cefef37932712022340a227ee498c4fab7f65520")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5aaa9f8b9b2efc9ca78d96162d5b52a61243a14")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
